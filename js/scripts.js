39df47519607ff530d2d7f7d24773fb0455632d2
Natalie Benjes <natalie.benjes@gmail.com>
2023-06-09 08:58:44 -0700

    add first test. numberReplacer returns an array of numbers from 0 to the user's inputted number

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+function numberReplacer(numero) {
+  let endNumber = numero;
+  let numberArray = [];
+  for (let i = 0; i <= endNumber; i +=1) {
+    numberArray.push(i);
+  }
+  return numberArray;
+}
