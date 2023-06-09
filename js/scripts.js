2f5361e9028fec05953e73082fe06c34f0c45a08
Natalie Benjes <natalie.benjes@gmail.com>
2023-06-09 09:39:19 -0700

    number replacer adds returns an array of strings

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,16 @@
+function containsThree()
+
 function numberReplacer(numero) {
   let endNumber = numero;
   let numberArray = [];
   for (let i = 0; i <= endNumber; i +=1) {
+    
     numberArray.push(i);
   }
-  return numberArray;
+
+  let stringArray = [];
+  numberArray.forEach(function(num) {
+    stringArray.push(num.toString());
+  });
+  return stringArray;
 }
